f97d3889fcf68d786d091dd6f39d955ce576eae0
Astha Rastogi <asthar@scc-gf1.scc.bu.edu>
2025-03-17 16:50:12 -0400

    modified pretrained model evaluation script for SCC

--- external/spoc/scripts/eval_pretrained_model.sh
+++ external/spoc/scripts/eval_pretrained_model.sh
@@ -33,9 +33,16 @@ else
 fi
 
 echo "Start evaluatioon"
+# python -m training.offline.online_eval --shuffle --eval_subset minival --output_basedir tmp_log \
+#  --test_augmentation --task_type ObjectNavType \
+#  --input_sensors raw_navigation_camera raw_manipulation_camera last_actions an_object_is_in_hand \
+#  nav_task_relevant_object_bbox manip_task_relevant_object_bbox nav_accurate_object_bbox manip_accurate_object_bbox \
+#  --house_set objaverse --wandb_logging False --num_workers 10 \
+#  --gpu_devices 0 1 --training_run_id SigLIP-ViTb-3-double-det-CHORES-S --local_checkpoint_dir $CKPT_DIR
 python -m training.offline.online_eval --shuffle --eval_subset minival --output_basedir tmp_log \
- --test_augmentation --task_type ObjectNavType \
+ --test_augmentation --task_type ObjectNavLocalRef \
  --input_sensors raw_navigation_camera raw_manipulation_camera last_actions an_object_is_in_hand \
  nav_task_relevant_object_bbox manip_task_relevant_object_bbox nav_accurate_object_bbox manip_accurate_object_bbox \
- --house_set objaverse --wandb_logging False --num_workers 10 \
- --gpu_devices 0 1 --training_run_id SigLIP-ViTb-3-double-det-CHORES-S --local_checkpoint_dir $CKPT_DIR
+ --house_set objaverse --wandb_logging False --num_workers 2 \
+ --gpu_devices 0 1 --training_run_id SigLIP-ViTb-3-double-det-CHORES-S --local_checkpoint_dir $CKPT_DIR 
+#  --eval_set_size 50
